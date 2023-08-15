887125dda293c971cc38d94edc536c6244fa3c92
yetzabeth <yetzaher@gmail.com>
2023-08-15 05:26:55 -0700

    test corrections

--- functions.c
+++ functions.c
@@ -8,28 +8,27 @@
 
 char *print_prompt(void)
 {
-    ssize_t prompt;
-    char *buffer = NULL;
-    size_t buffsize = 0;
+	ssize_t prompt;
+	char *buffer = NULL;
+	size_t buffsize = 0;
 
-    if (isatty(STDIN_FILENO) == 1)
-            write(STDOUT_FILENO,"$ ", 2);
-            
-    prompt = getline(&buffer, &buffsize, stdin);
-        
-    if (prompt == -1)
-    {
-        if (buffer)
-            free(buffer);
-        exit(EXIT_FAILURE);
-    }
-    if (buffer[prompt - 1] == '\n')
-    {
-        buffer[prompt - 1] = '\0';
-        prompt--;
-    }
+	if (isatty(STDIN_FILENO) == 1)
+		write(STDOUT_FILENO, "$ ", 2);
 
-    return (buffer);
+	prompt = getline(&buffer, &buffsize, stdin);
+
+	if (prompt == -1)
+	{
+		if (buffer)
+			free(buffer);
+		exit(EXIT_FAILURE);
+	}
+	if (buffer[prompt - 1] == '\n')
+	{
+		buffer[prompt - 1] = '\0';
+		prompt--;
+	}
+	return (buffer);
 }
 
 /**
@@ -42,60 +41,51 @@ char *print_prompt(void)
 
 char **parser(char *str, char *separator)
 {
-    char **tokens = NULL, *token = NULL, *aux = NULL;
-    int i, count = 0, j;
+	char **tokens = NULL, *token = NULL, *aux = NULL;
+	int i, count = 0, j;
 
-    if (str)
-    {
-        for (i = 0; str[i]; i++)
-        {
-            for (j = 0; separator[j]; j++)
-            {
-                if (str[i] == separator[j] || str[i + 1] == '\0')
-                    count++;
-            }
-        }
+	if (!str)
+		return (NULL);
 
-        count += 1;
-        tokens = malloc(count * sizeof(char *));
+	for (i = 0; str[i]; i++)
+		for (j = 0; separator[j]; j++)
+			if (str[i] == separator[j] || str[i + 1] == '\0')
+				count++;
 
-        if (!tokens)
-        {
-            perror("Unable to malloc space\n");
-            return (NULL);
-        }
-        aux = strdup(str);
-        if (!aux)
-        {
-            perror("Unable to malloc space\n");
-            free(tokens);
-            return (NULL);
-        }
-        token = strtok(aux, separator);   
-        
-        for (i = 0; token; i++)
-        {
-            tokens[i] = strdup(token);
-            if (!tokens[i])
-            {
-                perror("Unable to malloc space\n");
-                for (j = 0; j < i; j++)
-                    free(tokens[j]);
-                free(tokens);
-                free(aux);
-                return NULL;
-            }
-            token = strtok(NULL, separator);
-        }
-        tokens[i] = NULL;
+	tokens = malloc((count + 1) * sizeof(char *));
+	if (!tokens)
+		perror("Unable to malloc space\n");
+		return (NULL);
 
-        free(aux);
-    }
-    return (tokens);
+	aux = strdup(str);
+	if (!aux)
+		perror("Unable to malloc space\n");
+		free(tokens);
+		return (NULL);
+
+	token = strtok(aux, separator);
+	for (i = 0; token; i++)
+	{
+		tokens[i] = strdup(token);
+		{
+			if (!tokens[i])
+			perror("Unable to malloc space\n");
+		for (j = 0; j < i; j++)
+			free(tokens[j]);
+			free(tokens);
+			free(aux);
+		}
+		return (NULL);
+	}
+	token = strtok(NULL, separator);
+	tokens[i] = NULL;
+
+	free(aux);
+	return (tokens);
 }
 
 /**
- * _getenv - Get the value of an environment variable.
+ * q* _getenv - Get the value of an environment variable.
  * @str: The name of the environment variable.
  *
  * Return: The value of the environment variable.
@@ -103,23 +93,22 @@ char **parser(char *str, char *separator)
 
 char *_getenv(char *str)
 {
-    int i;
-    char **parsed = NULL, *value = NULL;
-
-    for (i = 0; environ[i]; i++)
-    {
-        if ((strncmp(str, environ[i], strlen(str))) == 0)
-        {
-            parsed = parser(environ[i], "=");
-            value = strdup(parsed[1]);
-            for (i = 0; parsed[i]; i++)
-                free(parsed[i]);
-            free(parsed);
-            return (value);
-        }
-    }
-
-    return (NULL);
+	int i;
+	char **parsed = NULL, *value = NULL;
+	
+	for (i = 0; environ[i]; i++)
+	{
+		if ((strncmp(str, environ[i], strlen(str))) == 0)
+		{
+			parsed = parser(environ[i], "=");
+			value = strdup(parsed[1]);
+			for (i = 0; parsed[i]; i++)
+				free(parsed[i]);
+			free(parsed);
+			return (value);
+		}
+	}
+	return (NULL);
 }
 
 /**
@@ -131,46 +120,46 @@ char *_getenv(char *str)
 
 char *search_cmd(char *cmd)
 {
-    char *full_path = _getenv("PATH="), *path = NULL;
-    char **directories = parser(full_path, ":"); 
-    int i;
+	char *full_path = _getenv("PATH="), *path = NULL;
+	char **directories = parser(full_path, ":");
+	int i;
+	
+	if (!full_path || !directories)
+	{
+		perror("Error parsing directories");
+		free(full_path);
+		return NULL;
+	}
+	
+	for (i = 0; directories[i]; i++)
+	{
+		path = (char *)calloc((strlen(directories[i]) + strlen(cmd) + 2), sizeof(char));
+		if (!path)
+		{
+			perror("Unable to malloc space\n");
+			for (i = 0; directories[i]; i++)
+				free(directories[i]);
+			free(directories);
+			free(full_path);
+			return (NULL);
+		}
+		strcat(path, directories[i]); strcat(path, "/"); strcat(path, cmd);
+		if (access(path, F_OK) == 0)
+		{
+			for (i = 0; directories[i]; i++)
+				free(directories[i]);
+			free(directories);
+			free(full_path);
+			return (path);
+		}
+		free(path);
+	}
+	for (i = 0; directories[i]; i++)
+		free(directories[i]);
+	free(directories);
+	free(full_path);
 
-    if (!full_path || !directories)
-    {
-        perror("Error parsing directories");
-        free(full_path);
-        return NULL;
-    }
-     
-    for (i = 0; directories[i]; i++)
-    {
-        path = (char *)calloc((strlen(directories[i]) + strlen(cmd) + 2), sizeof(char));
-        if (!path)
-        {
-            perror("Unable to malloc space\n");
-            for (i = 0; directories[i]; i++)
-                free(directories[i]);
-            free(directories);
-            free(full_path);
-            return (NULL);
-        }
-        strcat(path, directories[i]); strcat(path, "/"); strcat(path, cmd);
-        if (access(path, F_OK) == 0)
-        {
-            for (i = 0; directories[i]; i++)
-                free(directories[i]);
-            free(directories);
-            free(full_path);
-            return (path);
-        }            
-        free(path);
-    }
-    for (i = 0; directories[i]; i++)
-        free(directories[i]);
-    free(directories);
-    free(full_path);
-    
-    return (NULL);
+	return (NULL);
 }
 
 /**
@@ -181,47 +170,45 @@ char *search_cmd(char *cmd)
 
 void create_process(char **buff, int count)
 {
-    int status = 0;
-    char *full_path = NULL;
-    struct stat path_stat;
-    pid_t pid;
+	int status = 0;
+	char *full_path = NULL;
+	struct stat path_stat;
+	pid_t pid;
 
     if (buff[0][0] == '/')
     {
-        if (stat(buff[0], &path_stat) != 0 || !(path_stat.st_mode & S_IXUSR))
-        {
-            dprintf(STDERR_FILENO,"%s: %d: not found\n", buff[0], count);
-            return;
+		if (stat(buff[0], &path_stat) != 0 || !(path_stat.st_mode & S_IXUSR))
+		{
+			dprintf(STDERR_FILENO,"%s: %d: not found\n", buff[0], count);
+			return;
+		}
+	}
+	else
+	{
+		full_path = search_cmd(buff[0]);
+		if (!full_path)
+		{
+			dprintf(STDERR_FILENO, "%s: %d: not found\n", buff[0], count);
+			return;
+		}
+		
+		pid = fork();
+		if (pid == -1)
+			perror("Can't fork");
+		else if (pid == 0)
+		{
+			if (full_path){
+				execve(full_path, buff, environ);
+				free(full_path);
         }
-    }
-    else
-    {
-        full_path = search_cmd(buff[0]);
-        if (!full_path)
-        {
-            dprintf(STDERR_FILENO, "%s: %d: not found\n", buff[0], count);
-            return;
-        }
-    }
-
-    pid = fork();
-    if (pid == -1)
-        perror("Can't fork");
-    else if (pid == 0)
-    {
-        if (full_path)
-        {
-            execve(full_path, buff, environ);
-            free(full_path);
-        } 
         else
-            execve(buff[0], buff, environ);
-        perror("Error in execve");
-        free(buff);
-        exit(EXIT_FAILURE);
-    }
-    else
-    {
+			execve(buff[0], buff, environ);
+		perror("Error in execve");
+		free(buff);
+		exit(EXIT_FAILURE);
+		}
+		else
+		{
         free(full_path);
         waitpid(pid, &status, 0);
     }
